5547ed5672c7370bf85a5628132058abf530f14a
Henrique Venancio <henriquemarinho12@gmail.com>
2022-05-08 18:28:50 -0300

    Front End completo

--- main.js
+++ main.js
@@ -1,9 +1,49 @@
-window.addEventListener('scroll',onScroll)
+window.addEventListener('scroll', onScroll)
 
 onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  console.log(
+    'O topo da seção chegou ou passou da linha?',
+    sectionTopReachOrPassedTargetLine
+  )
+
+  // a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // o final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  console.log('O fundo da seção passou da linha?', sectionEndPassedTargetLine)
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
